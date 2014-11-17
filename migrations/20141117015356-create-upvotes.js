3d850fdacf91c399e1907762e880705c43ea5c10
Mike Lyons <mlyons@mkone.co>
2014-11-17 01:56:21 -0500

    Added upvotes and downvotes tables

---
+++ migrations/20141117015356-create-upvotes.js
@@ -0,0 +1,38 @@
+module.exports = {
+    up: function( migration, DataTypes, done ) {
+        migration
+            .createTable( 'Upvotes', {
+                id: {
+                    type: DataTypes.INTEGER,
+                    primaryKey: true,
+                    autoIncrement: true
+                },
+                rateable_id: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false
+                },
+                user_id: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false
+                },
+                created_at: {
+                    type: DataTypes.DATE,
+                    allowNull: false
+                },
+                updated_at: {
+                    type: DataTypes.DATE,
+                    allowNull: false
+                },
+                deleted_at: {
+                    type: DataTypes.DATE,
+                    allowNull: true
+                }
+            } )
+            .complete( done );
+    },
+    down: function( migration, DataTypes, done ) {
+        migration
+            .dropTable( 'Upvotes' )
+            .complete( done );
+    }
+};
