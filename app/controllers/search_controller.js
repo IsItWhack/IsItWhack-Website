26a35e14dd0aa18003a2b1cdcb08cb4020dd49b1
Mike Lyons <mlyons@mkone.co>
2014-11-17 03:25:09 -0500

    Added very basic redirect to search bar

---
+++ app/controllers/search_controller.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright of IsItWhack.com
+ *
+ * Authors:
+ *     - Mike Lyons (m@IsItWhack.com)
+ */
+
+(function() {
+    'use strict';
+
+    var db = require( '../models' ).db,
+        Rateable = db.Rateable;
+
+    var controller = require( './' ),
+        rateable_controller = require( './rateable_controller' ),
+        q = require( 'q' ),
+        WebError = require( '../../lib/error' ).WebError;
+
+    var url_name = 'search';
+    module.exports.url_name = url_name;
+
+    var search = function( req, res, next ) {
+        res.redirect( 'http://interstellar.' + req.headers.host );
+    };
+
+    module.exports.addRoutes = function( app ) {
+        app.post( '/' + url_name, search );
+    };
+})();
