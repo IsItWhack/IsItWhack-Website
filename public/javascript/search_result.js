cf1e443897c74e4c407eac90be00bcda07f3248e
Mike Lyons <mdl0394@gmail.com>
2014-12-20 20:21:23 -0500

    Added scrolling code

--- public/javascript/search_result.js
+++ public/javascript/search_result.js
@@ -8,6 +8,33 @@
 (function() {
     'use strict';
 
+    var lastScrollTop = 0,
+        SCROLL_SPEED = 200,
+        scrolling = false;
 
+    var startScroll = function( target ) {
+        if( scrolling !== target ) {
+            scrolling = target;
+
+            $('html, body').stop().animate({
+                scrollTop: $(target).offset().top
+            }, SCROLL_SPEED, function() {
+                scrolling = false;
+            } );
+        }
+    };
+
+    $( document ).ready( function() {
+        lastScrollTop = $(this).scrollTop();
+        $( window ).scroll( function( mes ) {
+            var scrollTop = $(this ).scrollTop();
+            if( scrollTop < lastScrollTop ) { // Scroll up
+                startScroll( "#search_main" );
+            } else { // Scroll down
+                startScroll( "#search_after" );
+            }
+            lastScrollTop = scrollTop;
+        } );
+    } );
 
 }) ();
